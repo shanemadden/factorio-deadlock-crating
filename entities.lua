c5b3f169b4a4f80bdf4140b14cac78de6267bdda
Deadlock989 <matt.brown.se17@googlemail.com>
2019-06-06 19:02:41 +0100

    Delete entities.lua

--- entities.lua
+++
@@ -1,5 +0,0 @@
-local DCM = require "prototypes.shared"
-
-for tier = 1,DCM.TIERS do
-	DCM.create_machine_entity(tier, nil, nil, nil, nil, nil, (tier < DCM.TIERS) and (DCM.MACHINE_PREFIX..(tier+1)) or nil, nil)
-end
